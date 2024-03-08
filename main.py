80ff4ffda5c2f17e818cdd490c466f61dbc4f129
•••MisterZafarov <110981143+mrzafarov@users.noreply.github.com>
2024-03-08 17:06:29 +0400

    Update main.py
    Signed-off-by: •••MisterZafarov <110981143+mrzafarov@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import logging
 import wikipedia
 from aiogram import Bot, Dispatcher, executor, types
 
-API_TOKEN = 'BOT API TOKENI'
+API_TOKEN = '6764781769:AAE6g4k5TXpmrDn24P2LNsz6R7sr8zM5Zqw'
 
 wikipedia.set_lang("uz")
 
-logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
@@ -14,7 +12,7 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     await message.reply("WikiBot'ga xush kelibsiz!")
-    
+
 
 @dp.message_handler(commands=['help'])
 async def send_help(message: types.Message):
@@ -24,10 +22,10 @@ async def send_help(message: types.Message):
 @dp.message_handler()
 async def sendWiki(message: types.Message):
     try:
-        javob = wikipedia.summary(message.text)
-        await message.answer(javob)
+        answer = wikipedia.summary(message.text)
+        await message.answer(answer)
     except:
-        await message.answer('Bu mavzuga oid maqola topilmadi!')
+        await message.answer("Bu mavzuga oid maqola topilmadi!")
 
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+
+executor.start_polling(dp, skip_updates=True)
